8c8ef00e5aa33273ddf68519365c84706d4593a8
Dominik Stefancik <dstefancik@gmail.com>
2022-05-24 11:35:28 +0200

    Create skeleton of a React app

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'es5',
+  singleQuote: true,
+  semi: true,
+  printWidth: 100,
+  quoteProps: 'consistent',
+  tabWidth: 2,
+  bracketSpacing: true,
+};
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return <div className="App">REACT APP</div>;
+}
+
+export default App;
