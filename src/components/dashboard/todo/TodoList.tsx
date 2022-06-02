61cc8bc4f7fedc1a87fcb982dff118c57c9a014a
Dominik Stefancik <dstefancik@gmail.com>
2022-06-02 12:58:54 +0200

    Add spinner when loading Todo items

--- src/components/dashboard/todo/TodoList.tsx
+++ src/components/dashboard/todo/TodoList.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { axiosPrivate, getRequestConfig } from '../../../api/axios';
 import { API_TODOS_PATH } from '../../../api/url';
 import useAbortController from '../../../hooks/useAbortController';
 import useAuth from '../../../hooks/useAuth';
+import { SpinnerIcon } from '../../styled/Icon';
 import { TodoListPanel } from '../styled/Panel';
 import TodoItem from './TodoItem';
 import { Item } from './types';
@@ -13,6 +16,8 @@ interface TodoListProps {
 }
 
 const TodoList = ({ todoList, setTodoList }: TodoListProps) => {
+  // represents the state when a page is loading a data (e.g. the component waiting for the response from a backend)
+  const [isLoading, setIsLoading] = useState(false);
   const { authUser } = useAuth();
   const controller = useAbortController();
 
@@ -20,6 +25,8 @@ const TodoList = ({ todoList, setTodoList }: TodoListProps) => {
     let isMounted = true;
 
     const getTodos = async () => {
+      setIsLoading(true);
+
       try {
         const response = await axiosPrivate.get(
           API_TODOS_PATH,
@@ -27,7 +34,11 @@ const TodoList = ({ todoList, setTodoList }: TodoListProps) => {
         );
 
         isMounted && setTodoList(response.data);
-      } catch (error) {}
+      } catch (error) {
+      } finally {
+        // set the loading state to false after we received a response from a backend
+        setIsLoading(false);
+      }
     };
 
     getTodos();
@@ -43,6 +54,11 @@ const TodoList = ({ todoList, setTodoList }: TodoListProps) => {
 
   return (
     <TodoListPanel>
+      {isLoading && (
+        <SpinnerIcon>
+          <FontAwesomeIcon icon={faSpinner} pulse />
+        </SpinnerIcon>
+      )}
       {todoList &&
         todoList.map((item) => (
           <TodoItem key={item.id} todoItem={item} todoList={todoList} setTodoList={setTodoList} />
